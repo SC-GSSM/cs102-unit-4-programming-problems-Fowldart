9b3a8aae1612d79478ee40ba35754ddfe418050d
Fowldart <136510935+Fowldart@users.noreply.github.com>
2026-03-24 01:16:55 +0000

    again plz

--- src/main/java/unit4/Unit4.java
+++ src/main/java/unit4/Unit4.java
@@ -200,7 +200,7 @@ public class Unit4 {
         }
 
         for(int i = 1; i <= n; i++){
-            for(int j = 0; j<i; J++){
+            for(int j = 0; j<i; j++){
                 System.out.print("*");
             }
             System.out.print("\n");
